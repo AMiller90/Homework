cffea53161e8a434c5a99d8c2860de8084d98e5e
AMiller90 <A_Miller729@yahoo.com>
2015-08-19 12:07:41 -0500

    Fixed sln
    Fixed sln problems

---
+++ Array Project 2/Array Project 2/Array Project 2.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+	std::string name;
+
+	std::cout << "Please Into Your First And Last Name:" << std::endl;
+	std::getline(std::cin, name);
+
+	std::cout << "Your Name Is: " << name << std::endl;
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
