c72285fc6d480a37c49136d2695dc54fab78159c
AMiller90 <A_Miller729@yahoo.com>
2015-08-27 22:44:23 -0700

    update

--- Name Project Assignment/Name Project Assignment/Name Project Assignment.cpp
+++ Name Project Assignment/Name Project Assignment/Name Project Assignment.cpp
@@ -7,62 +7,66 @@
 
 
 
-void converttoASCII(char name[]) {
+void converttoASCII(char name[], int ASCII []) {
 
-	int ASCII[50];
 	
-	//Loops through the name input
-	for (int i = 0; i < name[i]; i++)
-	{
-		//checks for index equal to space and sets the index before the space to a smiley face
-		if ((name[i] == (char)32))
+		//Loops through the name input
+		for (int i = 0; i < name[i]; i++)
 		{
-			name[i - 1] = (char)1;
-		}
+			//sets name to ascii characters
+			ASCII[i] = name[i];
 
-		//checks for index of last character at end of line and sets it to smiley face
-		if (i == strlen(name) - 1)
-		{
-			name[i] = (char)1;
 		}
-
-		////Type cast the char array to its ASCII equivalent
-		ASCII[i] = name[i];
-	
-	
-	}
-	
 	
 		
 
-
 	}
 
-void converttoString(char name[])
+void converttoString(char name[], int ASCII[])
 {
-	//Calls The ASCII Conversion
-	converttoASCII(name);
+
 
 	char fullName[3][30];
 
 	//Loops Through Input And Sets It To 2D Array
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < strlen(name); j++)
+
+		for (int j = 0; j < ASCII[j]; j++)
 		{
-			fullName[i][j] = name[j];
+		
+			////checks for index equal to space and sets the index before the space to a smiley face
+			if (name[j] == (char)32)
+			{
+				name[j - 1] = 1;
+			}
+			//checks for index of last character at end of line and sets it to smiley face
+			if (j == strlen(name) - 1)
+			{
+				name[j] = (char)1;
+			}
 
 			
+			fullName[i][j] = name[j];
+
+
 		}
+
 
 
 	}
 
+
+
+
+
 	//Loops Through 2D Array And Prints Data
-	for (int i = 0; i < 1; i++)
+	for (int i = 2; i < 3; i++)
 	{
-		for (int j = 0; j < strlen(name); j++)
+		for (int j = 0; j < ASCII[j]; j++)
 		{
+			
+
 			std::cout << fullName[i][j];
 
 
@@ -70,20 +74,30 @@ void converttoString(char name[])
 		std::cout << std::endl;
 	}
 
-	
-}
+	}
 int main() {
 
-	
+
+	int ASCII[50];
 	char name[50];
 	std::cout << "What Is Your Name?\n";
 	std::cin.getline(name, 50);
 
 	std::cout << std::endl;
 
-	converttoString(name);
-	
+	//Calls The ASCII Conversion
+	converttoASCII(name, ASCII);
+
+    converttoString(name, ASCII);
+
 
 	system("PAUSE");
 	return 0;
 }
+
+
+
+
+
+
+
