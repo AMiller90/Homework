ca7ecd97bf3f1634170037b429ebc2484c993d67
AMiller90 <A_Miller729@yahoo.com>
2015-09-22 20:47:36 -0700

    update
    update

--- Character Arrays/Array Project 6/Array Project 6/Array Poject 6.cpp
+++ Character Arrays/Array Project 6/Array Project 6/Array Poject 6.cpp
@@ -2,39 +2,31 @@
 
 #include<string>
 #include<iostream>
-#include<string>
+
 
 int main() {
 
 
 
-	char name1[6] = { 'h', 'e', 'l', 'l', 'o' };
-	char name2[6] = { 'w', 'o', 'r', 'l', 'd' };
+	char name[20] = { };
+	char password[20] = { };
 
-	char name3[2][40] = { name1[6] + name2[5] };
+	char identifier[40] = {};
 
-	for (int i = 0; name2[i] != '\0'; i++){
-		
-			//std::cout << name1[i];
-			//std::cout << name2[i];
-			//std::cout << name3[i];
-	}
-	/*bool isdone = true;
+
+	bool isdone = true;
 
 	while (isdone == true){
 		std::cout << "Please Enter A Username: It must contain a sequence of 'usr' " << std::endl;
 		std::cout << std::endl;
-		std::string username;
-		std::getline(std::cin, username);
+		std::cin >> name;
 
 		int flag = 0;
-		for (unsigned int i = 0; i < username.length(); i++) {
-			if (username.at(i) == 'u') {
-				if (username.at(i + 1) == 's'){
-					if (username.at(i + 2) == 'r') {
+		for (unsigned int i = 0; i < strlen(name); i++) {
+			if (name[i] == 'u') {
+				if (name[i + 1] == 's'){
+					if (name[i + 2] == 'r') {
 						flag = 1;
-						std::cout << std::endl;
-						//std::cout << "Valid Name" << std::endl;
 						std::cout << std::endl;
 						break;
 					}
@@ -50,29 +42,23 @@ int main() {
 			return false;
 		}
 
-
+		flag = 0;
 		std::cout << "Please Enter This Password: P@55w0rd" << std::endl;
 		std::cout << std::endl;
-		std::string password;
-		std::getline(std::cin, password);
+		std::cin >> password;
 
-		for (unsigned int i = 0; i < password.length(); i++) {
-		if (password.at(i) == 'P') {
-		if (password.at(i + 1) == '@'){
-		if (password.at(i + 2) == '5') {
-		if (password.at(i + 3) == '5') {
-		if (password.at(i + 4) == 'w') {
-		if (password.at(i + 5) == '0'){
-		if (password.at(i + 6) == 'r'){
-		if (password.at(i + 7) == 'd'){
+		for (unsigned int i = 0; i < strlen(password); i++) {
+		if (password[i] == 'P') {
+		if (password[i + 1] == '@'){
+		if (password[i + 2] == '5') {
+		if (password[i + 3] == '5') {
+		if (password[i + 4] == 'w') {
+		if (password[i + 5] == '0'){
+		if (password[i + 6] == 'r'){
+		if (password[i + 7] == 'd'){
 			flag = 1;
 			std::cout << std::endl;
-			//std::cout << "Good Job!" << std::endl;
-			//system("PAUSE");
-			//return false;
-		
-		
-		       
+			break;
 		}
 		      }
 		     }
@@ -88,11 +74,21 @@ int main() {
 			return false;
 		}
 
-		std::cout << "This Is Your identifier: " << username << '-' << password << std::endl;
+		strcat_s(identifier, name);
+		strcat_s(identifier, "-");
+		strcat_s(identifier, password);
+		
+		std::cout << "Your identifier is: ";
+		for (int i = 0; i < strlen(identifier); i++)
+		{
+			std::cout << identifier[i];
+		}
 		std::cout << std::endl;
+
 		system("PAUSE");
 		return false;
-}*/
+}
+
 		system("PAUSE");
 		return 0;
 	
