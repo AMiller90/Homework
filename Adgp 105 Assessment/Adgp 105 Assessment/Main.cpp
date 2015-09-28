d9ac9110051d28b2e3af4cbf8853d10b58ab64da
AMiller90 <A_Miller729@yahoo.com>
2015-09-28 09:41:02 -0500

    adgp105
    adgp105

---
+++ Adgp 105 Assessment/Adgp 105 Assessment/Main.cpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//File: Main.cpp
+//Author: Andrew Miller
+//Date Created: 09/27/2015
+//Brief: This is the Main file. It will run the program from inside the main function.
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+//Includes The input/output stream to interact with the user and get input as well
+//as output to the console window.
+#include<iostream>
+
+//Using namespce std Globally So std Does Not Have To Be Used Each And Every Time When It Is Needed.
+using namespace std;
+
+//Main Function Where The Program Is Started And Ended
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+
+}
---
+++ Adgp 105 Assessment/Adgp 105 Assessment/Player.cpp
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//File: Player.cpp
+//Author: Andrew Miller
+//Date Created: 09/27/2015
+//Brief: This is the Player cpp file. It will contain all the Player Class function definitions and variable definitions.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "Player.h"
+
+
+Player::Player()
+{
+	m_iX = 0;
+	m_iY = 0;
+
+}
+
+
+Player::~Player()
+{
+}
+
+int Player::GetPositionX()
+{
+	return m_iX;
+}
+
+int Player::GetPositionY()
+{
+	return m_iY;
+}
+
+void Player::SetPosition(int a_iX, int a_iY)
+{
+	m_iX = a_iX;
+	m_iY = a_iY;
+
+}
