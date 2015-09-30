dcdc6f544e67fded16306ce6e5eb99dade5ee1d5
AMiller90 <A_Miller729@yahoo.com>
2015-09-30 13:55:25 -0500

    update
    update

---
+++ Stack Project/Stack Project/Stack.cpp
@@ -0,0 +1,24 @@
+#include "Stack.h"
+
+struct Node
+{
+	int num;
+	Node* top;
+};
+
+Stack::Stack()
+{
+
+}
+
+
+Stack::~Stack()
+{
+
+}
+
+void Stack::Push(Node &n)
+{
+	Node* top;
+	top = n;
+}
---
+++ Stack Project/Stack Project/Stack.h
@@ -0,0 +1,19 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+class Stack
+{
+public:
+
+	
+
+	Stack();
+	~Stack();
+
+	void Push(Node &n);
+	
+
+
+};
+
+#endif
