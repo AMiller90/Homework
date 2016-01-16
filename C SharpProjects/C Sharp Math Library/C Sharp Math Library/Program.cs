bd7ebfb95412e07fa65407e2961cd2df315e0bb5
AMiller90 <A_Miller729@yahoo.com>
2016-01-16 02:11:40 -0800

    Added Generics
    Added Generics

--- C SharpProjects/C Sharp Math Library/C Sharp Math Library/Program.cs
+++ C SharpProjects/C Sharp Math Library/C Sharp Math Library/Program.cs
@@ -10,41 +10,56 @@ namespace C_Sharp_Math_Library
     class Program
     {
 
-     class Vector2
+        class Vector2<T>
         {
 
-            public int X;
-            public int Y;
+            public T X;
+            public T Y;
 
-            public Vector2(int X, int Y)
+            public Vector2(T X, T Y)
             {
                 this.X = X;
                 this.Y = Y;
             }
 
-            public static Vector2 operator +(Vector2 a, Vector2 b)
-            {
-                return new Vector2(a.X + b.X, a.Y + b.Y);
+            public static Vector2<T> operator +(Vector2<T> a, Vector2<T> b)
+            {//Checks At Runtime 
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector2<T>(d_a.X + d_b.X, d_a.Y + d_b.Y);
             }
 
-            public static Vector2 operator -(Vector2 a, Vector2 b)
+            public static Vector2<T> operator -(Vector2<T> a, Vector2<T> b)
             {
-                return new Vector2(a.X - b.X, a.Y - b.Y);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector2<T>(d_a.X - d_b.X, d_a.Y - d_b.Y);
             }
 
-            public static Vector2 operator *(Vector2 a, Vector2 b)
+            public static Vector2<T> operator *(Vector2<T> a, Vector2<T> b)
             {
-                return new Vector2(a.X * b.X, a.Y * b.Y);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector2<T>(d_a.X * d_b.X, d_a.Y * d_b.Y);
             }
 
-            public static Vector2 operator /(Vector2 a, Vector2 b)
+            public static Vector2<T> operator /(Vector2<T> a, Vector2<T> b)
             {
-                return new Vector2(a.X / b.X, a.Y / b.Y);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector2<T>(d_a.X / d_b.X, d_a.Y / d_b.Y);
             }
 
-            public static Vector2 operator %(Vector2 a, Vector2 b)
+            public static Vector2<T> operator %(Vector2<T> a, Vector2<T> b)
             {
-                return new Vector2(a.X % b.X, a.Y % b.Y);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector2<T>(d_a.X % d_b.X, d_a.Y % d_b.Y);
             }
 
             public override string ToString()
@@ -54,42 +69,57 @@ namespace C_Sharp_Math_Library
 
         }
 
-     class Vector3
+        class Vector3<T>
         {
-            public int X;
-            public int Y;
-            public int Z;
+            public T X;
+            public T Y;
+            public T Z;
 
-            public Vector3(int X, int Y, int Z)
+            public Vector3(T X, T Y, T Z)
             {
                 this.X = X;
                 this.Y = Y;
                 this.Z = Z;
             }
 
-            public static Vector3 operator +(Vector3 a, Vector3 b)
+            public static Vector3<T> operator +(Vector3<T> a, Vector3<T> b)
             {
-                return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector3<T>(d_a.X + d_b.X, d_a.Y + d_b.Y, d_a.Z + d_b.Z);
             }
 
-            public static Vector3 operator -(Vector3 a, Vector3 b)
+            public static Vector3<T> operator -(Vector3<T> a, Vector3<T> b)
             {
-                return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector3<T>(d_a.X - d_b.X, d_a.Y - d_b.Y, d_a.Z - d_b.Z);
             }
 
-            public static Vector3 operator *(Vector3 a, Vector3 b)
+            public static Vector3<T> operator *(Vector3<T> a, Vector3<T> b)
             {
-                return new Vector3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector3<T>(d_a.X * d_b.X, d_a.Y * d_b.Y, d_a.Z * d_b.Z);
             }
 
-            public static Vector3 operator /(Vector3 a, Vector3 b)
+            public static Vector3<T> operator /(Vector3<T> a, Vector3<T> b)
             {
-                return new Vector3(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector3<T>(d_a.X / d_b.X, d_a.Y / d_b.Y, d_a.Z / d_b.Z);
             }
 
-            public static Vector3 operator %(Vector3 a, Vector3 b)
+            public static Vector3<T> operator %(Vector3<T> a, Vector3<T> b)
             {
-                return new Vector3(a.X % b.X, a.Y % b.Y, a.Z % b.Z);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Vector3<T>(d_a.X % d_b.X, d_a.Y % d_b.Y, d_a.Z % d_b.Z);
             }
 
             public override string ToString()
@@ -99,14 +129,14 @@ namespace C_Sharp_Math_Library
 
         }
 
-     class Color
+        class Color<T>
         {
-            public int R;
-            public int G;
-            public int B;
-            public int A;
+            public T R;
+            public T G;
+            public T B;
+            public T A;
 
-            public Color(int R, int G, int B, int A)
+            public Color(T R, T G, T B, T A)
             {
                 this.R = R;
                 this.G = G;
@@ -115,50 +145,65 @@ namespace C_Sharp_Math_Library
 
             }
 
-            public static Color operator +(Color a, Color b)
+            public static Color<T> operator +(Color<T> a, Color<T> b)
             {
-                return new Color(a.R + b.R, a.G + b.G, a.B + b.B, a.A + b.A);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Color<T>(d_a.R + d_b.R, d_a.G + d_b.G, d_a.B + d_b.B, d_a.A + d_b.A);
             }
 
-            public static Color operator -(Color a, Color b)
+            public static Color<T> operator -(Color<T> a, Color<T> b)
             {
-                return new Color(a.R - b.R, a.G - b.G, a.B - b.B, a.A - b.A);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Color<T>(d_a.R - d_b.R, d_a.G - d_b.G, d_a.B - d_b.B, d_a.A - d_b.A);
             }
 
-            public static Color operator *(Color a, Color b)
+            public static Color<T> operator *(Color<T> a, Color<T> b)
             {
-                return new Color(a.R * b.R, a.G * b.G, a.B * b.B, a.A * b.A);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Color<T>(d_a.R * d_b.R, d_a.G * d_b.G, d_a.B * d_b.B, d_a.A * d_b.A);
             }
 
-            public static Color operator /(Color a, Color b)
+            public static Color<T> operator /(Color<T> a, Color<T> b)
             {
-                return new Color(a.R / b.R, a.G / b.G, a.B / b.B, a.A / b.A);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Color<T>(d_a.R / d_b.R, d_a.G / d_b.G, d_a.B / d_b.B, d_a.A / d_b.A);
             }
 
-            public static Color operator %(Color a, Color b)
+            public static Color<T> operator %(Color<T> a, Color<T> b)
             {
-                return new Color(a.R % b.R, a.G % b.G, a.B % b.B, a.A % b.A);
+                dynamic d_a = a;
+                dynamic d_b = b;
+
+                return new Color<T>(d_a.R % d_b.R, d_a.G % d_b.G, d_a.B % d_b.B, d_a.A % d_b.A);
             }
 
             public override string ToString()
             {
-                return (String.Format("{0},{1},{2}", R, G, B, A));
+                return (String.Format("{0},{1},{2},{3}", R, G, B, A));
             }
-
         }
+
 
 
         static void Main(string[] args)
         {
-   
-            Vector2 nvec = new Vector2(15, 10);
-            Vector2 nvec1 = new Vector2 (15, 10);
 
-            Vector2 Sum = nvec + nvec1;
-            Vector2 Difference = nvec - nvec1;
-            Vector2 Product = nvec * nvec1;
-            Vector2 Quotient = nvec / nvec1;
-            Vector2 Modulus = nvec % nvec1;
+            Vector2<int> nvec = new Vector2<int>(15, 10);
+            Vector2<int> nvec1 = new Vector2<int>(15, 10);
+
+            Vector2<int> Sum = nvec + nvec1;
+            Vector2<int> Difference = nvec - nvec1;
+            Vector2<int> Product = nvec * nvec1;
+            Vector2<int> Quotient = nvec / nvec1;
+            Vector2<int> Modulus = nvec % nvec1;
 
             Console.WriteLine("Vector 2 Math: ");
             Console.WriteLine("Sum is: ({0})", Sum);
@@ -167,14 +212,14 @@ namespace C_Sharp_Math_Library
             Console.WriteLine("Quotient is: ({0})", Quotient);
             Console.WriteLine("Modulus is: ({0})\n", Modulus);
 
-            Vector3 nvec2 = new Vector3(15, 10, 5);
-            Vector3 nvec3 = new Vector3(15, 10, 5);
+            Vector3<int> nvec2 = new Vector3<int>(15, 10, 5);
+            Vector3<int> nvec3 = new Vector3<int>(15, 10, 5);
 
-            Vector3 Sum2 = nvec2 + nvec3;
-            Vector3 Difference2 = nvec2 - nvec3;
-            Vector3 Product2 = nvec2 * nvec3;
-            Vector3 Quotient2 = nvec2 / nvec3;
-            Vector3 Modulus2 = nvec2 % nvec3;
+            Vector3<int> Sum2 = nvec2 + nvec3;
+            Vector3<int> Difference2 = nvec2 - nvec3;
+            Vector3<int> Product2 = nvec2 * nvec3;
+            Vector3<int> Quotient2 = nvec2 / nvec3;
+            Vector3<int> Modulus2 = nvec2 % nvec3;
 
             Console.WriteLine("Vector 3 Math: ");
             Console.WriteLine("Sum is: ({0})", Sum2);
@@ -184,14 +229,14 @@ namespace C_Sharp_Math_Library
             Console.WriteLine("Modulus is: ({0})\n", Modulus2);
 
 
-            Color white = new Color(255, 255, 255, 1);
-            Color black = new Color(1, 1, 1, 1);
+            Color<int> white = new Color<int>(255, 255, 255, 1);
+            Color<int> black = new Color<int>(1, 1, 1, 1);
 
-            Color Sum3 = white + black;
-            Color Difference3 = white - black;
-            Color Product3 = white * black;
-            Color Quotient3 = white / black;
-            Color Modulus3 = white % black;
+            Color<int> Sum3 = white + black;
+            Color<int> Difference3 = white - black;
+            Color<int> Product3 = white * black;
+            Color<int> Quotient3 = white / black;
+            Color<int> Modulus3 = white % black;
 
             Console.WriteLine("Color Math: ");
             Console.WriteLine("Sum is: ({0})", Sum3);
@@ -201,10 +246,8 @@ namespace C_Sharp_Math_Library
             Console.WriteLine("Modulus is: ({0})", Modulus3);
 
             Console.ReadLine();
-
-
         }
 
     }
-
 }
+
