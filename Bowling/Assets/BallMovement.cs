b8db80207a7b0385bbbe058a01bf46fa836859ac
Andrew Miller <A_Miller729@yahoo.com>
2016-04-12 14:59:20 -0500

    8 hour game part 2

---
+++ Bowling/Assets/BallMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+    float x;
+    float y;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        x -= Input.GetAxis("Mouse Y") * 5;
+        y += Input.GetAxis("Mouse X") * 5;
+
+        x = Mathf.Clamp(x, -90, 90);
+        //transform.rotation = Quaternion.Euler(x, y, 0);
+        gameObject.transform.rotation = Quaternion.Euler(0, y, 0);
+    }
+}
---
+++ Bowling/Assets/Bowl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bowl : MonoBehaviour {
+
+    bool isMoving;
+	// Use this for initialization
+	void Start ()
+    {
+        isMoving = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	   if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isMoving = true;
+
+        }
+        if (isMoving == true)
+        {
+            gameObject.transform.Translate(0.0f, 0.0f, 3.0f * Time.deltaTime * 5.0f);
+        }
+    }
+}
