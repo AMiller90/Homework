418bf5984862339d3d9fea504f750f8f2aafac9f
Andrew Miller <A_Miller729@yahoo.com>
2016-08-30 11:14:05 -0500

    Added Morning Assignment for 08/30/16
    Write a function that takes another function as an argument. Store this
    argument in an array. Then execute every function in the array

---
+++ Morning Assignments/FunctionAsAFunctionParameter/FunctionAsAFunctionParameter/Main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+//08/29/16
+//Write a function that takes another function as an argument.
+//Store this argument in an array. Then execute every function in the array
+
+//Function with a Function pointer as arguement
+void ExecuteFunctions(void(*funcptr)());
+
+void PrintStuff()
+{
+	std::cout << "I am a function to print stuff";
+}
+
+void(*functarray[1])();
+
+int main()
+{
+	//Call Function
+	ExecuteFunctions(PrintStuff);
+
+	int tmp;
+	std::cin >> tmp;
+
+}
+
+void ExecuteFunctions(void(*funcptr)())
+{
+	//Store into array
+	functarray[0] = funcptr;
+
+	//Doing sizeof(array) will get the total number of bytes allocated for the array.
+	//You can then find out the number of elements in the array by dividing the size of one element in the array sizeof(*array)
+	int size = sizeof(functarray) / sizeof(*functarray);
+
+	//Loop through and call functions
+	for (int i = 0; i < size; i++)
+	{
+		functarray[i]();
+	}
+}
